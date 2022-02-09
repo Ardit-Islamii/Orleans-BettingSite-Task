3945d8c9e8092a6e9aef041fe41aa10de0980d3d
Ardit Islami <ai43935@ubt-uni.net>
2022-02-09 14:39:03 +0100

    Basic event-streaming added.

--- Grains/BetGrain.cs
+++ Grains/BetGrain.cs
@@ -9,33 +9,47 @@ using Orleans_BettingSite_Task.Streams;
 namespace Orleans_BettingSite_Task.Grains
 {
     [StorageProvider(ProviderName = "amountStore")]
+    [ImplicitStreamSubscription("default")]
     public class BetGrain : Grain, IBetGrain
     {
         private IAsyncObservable<BetMessage> consumer;
-        internal int numConsumedItems;
         internal ILogger logger;
         private StreamSubscriptionHandle<BetMessage> consumerHandle;
 
         private readonly IPersistentState<AmountState> _amount;
 
-        public BetGrain([PersistentState("amount", "amountStore")] IPersistentState<AmountState> amount)
+        public BetGrain([PersistentState("amount", "amountStore")] IPersistentState<AmountState> amount, ILoggerFactory loggerFactory)
         {
             _amount = amount;
+            logger = loggerFactory.CreateLogger($"{this.GetType().Name}-{this.IdentityString}");
         }
-
         public override async Task OnActivateAsync()
         {
-            IStreamProvider streamProvider = this.GetStreamProvider("bet");
-            consumer = streamProvider.GetStream<BetMessage>(this.GetPrimaryKey(), "default");
-            consumerHandle = await consumer.SubscribeAsync(new StreamObserver());
-            await base.OnActivateAsync();
+            var streamProvider = GetStreamProvider("bet");
+            var stream = streamProvider.GetStream<BetMessage>(this.GetPrimaryKey(), "default");
+            await stream.SubscribeAsync(OnNextAsync, OnErrorAsync, OnCompletedAsync);
         }
-
+        public async Task OnNextAsync(BetMessage item, StreamSequenceToken token = null)
+        {
+            logger.Info("OnNextAsync({0}{1})", item, token != null ? token.ToString() : "null");
+            await SetBetAmountAsync(item.Amount);
+            await Task.CompletedTask;
+        }
+        public Task OnCompletedAsync()
+        {
+            logger.Info("OnCompletedAsync()");
+            return Task.CompletedTask;
+        }
+        public Task OnErrorAsync(Exception ex)
+        {
+            logger.Info("OnErrorAsync({0})", ex);
+            return Task.CompletedTask;
+        }
         public async Task<decimal> GetBetAmountAsync()
         {
-            return _amount.State.Amount;
+            var amount = _amount.State.Amount;
+            return amount;
         }
-
         public async Task<decimal> SetBetAmountAsync(decimal amount)
         {
             _amount.State.Amount = amount;
